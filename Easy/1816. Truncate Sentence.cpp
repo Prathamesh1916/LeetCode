858c3f20a67cf6dbddfb43c3731cf357a7a7dbc7
Prathamesh Tidke <108576909+Prathamesh1916@users.noreply.github.com>
2023-02-13 18:10:00 +0530

    Create 1816. Truncate Sentence.cpp

---
+++ Easy/1816. Truncate Sentence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int space = 0 ; 
+        int i = 0 ; 
+        int n = s.size() ; 
+        string res ="";
+        while ((i != n)){
+            if (s[i] == ' '){
+                space ++;
+            }
+            if(space == k){
+                break ;
+            }
+            res += s[i];   
+            i++; 
+        }
+        return res;
+    }
+};
